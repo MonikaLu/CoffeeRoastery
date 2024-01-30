994f47a49a1ea0fb3ad3c985c2d8ceb0b8367c3f
Monika Luu <monika-luu@hotmail.com>
2024-01-30 21:51:13 +0100

    Add more coffees to the menu

--- components/CoffeeMenu.js
+++ components/CoffeeMenu.js
@@ -1,25 +1,68 @@
 import React from "react";
 import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
 
-const EspressoImage = require("./../assets/espresso.jpeg");
-const CappuccinoImage = require("./../assets/cappuccino.jpeg");
+const CappuccinoImage = require("./../assets/cappuccino.png");
+const LatteImage = require("./../assets/latte.png");
+const MacchiatoImage = require("./../assets/macchiato.png");
+const EspressoImage = require("./../assets/espresso.png");
+const HotChocolateImage = require("./../assets/hot_chocolate.png");
+const AffogatoImage = require("./../assets/affogato.png");
+const AmericanoImage = require("./../assets/americano.png");
+const CortadoImage = require("./../assets/cortado.png");
+const MochaImage = require("./../assets/mocha2.png");
+const ChristmasMochaImage = require("./../assets/mocha.png");
 
 const CoffeeMenu = () => {
   const coffeeItems = [
     {
-      title: "Espresso",
-      imageUrl: EspressoImage,
+      title: "Affogato",
+      imageUrl: AffogatoImage,
+      price: 30,
+    },
+    {
+      title: "Americano",
+      imageUrl: AmericanoImage,
       price: 30,
     },
     {
       title: "Cappuccino",
       imageUrl: CappuccinoImage,
+      price: 30,
+    },
+    {
+      title: "Cortado",
+      imageUrl: CortadoImage,
+      price: 30,
+    },
+    {
+      title: "Latte",
+      imageUrl: LatteImage,
       price: 35,
     },
     {
-      title: "Cappuccino",
-      imageUrl: CappuccinoImage,
-      price: 35,
+      title: "Macchiato",
+      imageUrl: MacchiatoImage,
+      price: 50,
+    },
+    {
+      title: "Christmas Mocha",
+      imageUrl: ChristmasMochaImage,
+      price: 50,
+    },
+    {
+      title: "Mocha",
+      imageUrl: MochaImage,
+      price: 50,
+    },
+    {
+      title: "Espresso",
+      imageUrl: EspressoImage,
+      price: 25,
+    },
+    {
+      title: "Hot Chocolate",
+      imageUrl: HotChocolateImage,
+      price: 25,
     },
   ];
 
