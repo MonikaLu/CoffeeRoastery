e288626dc61eb8da0b097a284304d7fff702a4a5
Monika Luu <monika-luu@hotmail.com>
2024-02-02 10:52:40 +0100

    Refactor code

--- coffees.ts
+++ coffees.ts
@@ -1,75 +1,64 @@
 import { IMenuItem } from "./types/IMenuItem";
 import { MenuItemCategories } from "./types/MenuItemCategories";
 
-const CappuccinoImage = require("./assets/cappuccino.png");
-const LatteImage = require("./assets/latte.png");
-const MacchiatoImage = require("./assets/macchiato.png");
-const EspressoImage = require("./assets/espresso.png");
-const HotChocolateImage = require("./assets/hot_chocolate.png");
-const AffogatoImage = require("./assets/affogato.png");
-const AmericanoImage = require("./assets/americano.png");
-const CortadoImage = require("./assets/cortado.png");
-const MochaImage = require("./assets/mocha2.png");
-const ChristmasMochaImage = require("./assets/mocha.png");
-
 const MenuItemItems: IMenuItem[] = [
   {
     name: "Affogato",
-    coverUrl: AffogatoImage,
+    coverUrl: require("./assets/affogato.png"),
     price: 30,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Americano",
-    coverUrl: AmericanoImage,
+    coverUrl: require("./assets/americano.png"),
     price: 30,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Cappuccino",
-    coverUrl: CappuccinoImage,
+    coverUrl: require("./assets/cappuccino.png"),
     price: 30,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Cortado",
-    coverUrl: CortadoImage,
+    coverUrl: require("./assets/cortado.png"),
     price: 30,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Latte",
-    coverUrl: LatteImage,
+    coverUrl: require("./assets/latte.png"),
     price: 35,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Macchiato",
-    coverUrl: MacchiatoImage,
+    coverUrl: require("./assets/macchiato.png"),
     price: 50,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Christmas Mocha",
-    coverUrl: ChristmasMochaImage,
+    coverUrl: require("./assets/mocha.png"),
     price: 50,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Mocha",
-    coverUrl: MochaImage,
+    coverUrl: require("./assets/mocha2.png"),
     price: 50,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Espresso",
-    coverUrl: EspressoImage,
+    coverUrl: require("./assets/espresso.png"),
     price: 25,
     type: MenuItemCategories.HOT_DRINKS,
   },
   {
     name: "Hot Chocolate",
-    coverUrl: HotChocolateImage,
+    coverUrl: require("./assets/hot_chocolate.png"),
     price: 25,
     type: MenuItemCategories.HOT_DRINKS,
   },
